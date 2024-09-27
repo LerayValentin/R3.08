1b6d75c9b2ebeda73b6cebcd1e3f21eb344979cf
LerayValentin <148783160+LerayValentin@users.noreply.github.com>
2024-09-27 11:54:44 +0200

    Delete R3.08-main directory

--- R3.08-main/TP2/joueur.py
+++
@@ -1,6 +0,0 @@
-from personnage import Personnage
-class Joueur:
-    """
-    Classe Joueur
-    """
-    def __init__(self, nom, ):
--- R3.08-main/src/mmorpg/joueur.py
+++
@@ -1,3 +0,0 @@
-"""from personnage import Personnage
-class Joueur:
-    def __init__(self, nom, ):"""
